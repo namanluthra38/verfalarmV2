87d0ca4ad0392a5ff380030ba38fcbdc70d4a062
Naman <156838396+namanluthra38@users.noreply.github.com>
2026-02-13 19:16:14 +0530

    added frontend cors

--- src/main/java/com/verf/ProdExp/config/SecurityConfig.java
+++ src/main/java/com/verf/ProdExp/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://verfalarm.netlify.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);

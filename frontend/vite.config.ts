330d71dcd6cef8d4bcca14e49a57f6863837f22f
Naman <156838396+namanluthra38@users.noreply.github.com>
2026-03-01 18:35:30 +0530

    Add PWA setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Verfalarm',
+        short_name: 'Verfalarm',
+        theme_color: '#16a34a',
+        icons: [
+          {
+            src: 'icons/verfalarm-logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/verfalarm-logo',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
